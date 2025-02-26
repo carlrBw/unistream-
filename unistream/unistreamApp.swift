489ecb0128d7905c9448df34b2a7a62b191f6a27
Carl Baker-Williams <carlrenee.bakerw@gmail.com>
2025-02-26 16:17:59 -0600

    Initial Commit

---
+++ unistream/unistreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  unistreamApp.swift
+//  unistream
+//
+//  Created by Carl Baker-Williams on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct unistreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
